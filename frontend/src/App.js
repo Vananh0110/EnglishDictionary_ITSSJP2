71ddfef3506b2e4e22bf2ef2c05c13fdb1f51c84
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2024-06-05 15:29:08 +0700

    Format file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SearchPage from './pages/search/SearchPage';
+import ListTopics from './pages/topic/ListTopics';
 function App() {
-  return <div>English Dictionary</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<SearchPage />} />
+        <Route path="topics" element={<ListTopics />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/topic/ListTopics.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListTopics = () => {
+  return (
+    <div>
+      List Topics
+    </div>
+  )
+}
+
+export default ListTopics
