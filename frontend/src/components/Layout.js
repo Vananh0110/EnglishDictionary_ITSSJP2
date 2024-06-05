8cc8faaa42a378818a15dd9425178a914722d4de
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2024-06-05 15:59:57 +0700

    layout

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Layout, Menu } from "antd";
+const { Header, Content } = Layout;
+const items = [
+  {
+    key: 1,
+    label: `Search`,
+  },
+  {
+    key: 2,
+    label: `Topics`,
+  },
+];
+const AppLayout = ({ children, page = "1" }) => {
+  const navigate = useNavigate();
+  const handleChangePage = (value) => {
+    switch (value.key) {
+      case "1":
+        navigate("/");
+        break;
+      case "2":
+        navigate("/topics");
+        break;
+      default:
+        console.log("chuc nang chua kha dung");
+        break;
+    }
+    console.log(value);
+  };
+  return (
+    <Layout className='h-[100vh]'>
+      <Header
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+        className='mb-10'
+      >
+        <div className='demo-logo' />
+        <Menu
+          theme='dark'
+          mode='horizontal'
+          defaultSelectedKeys={[page]}
+          items={items}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+          onClick={handleChangePage}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        {children}
+      </Content>
+    </Layout>
+  );
+};
+export default AppLayout;
--- frontend/src/pages/topic/ListTopics.js
+++ frontend/src/pages/topic/ListTopics.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from "react";
+
+import Layout from "../../components/Layout";
 
 const ListTopics = () => {
-  return (
-    <div>
-      List Topics
-    </div>
-  )
-}
+  return <Layout page={"2"}>List Topics</Layout>;
+};
 
-export default ListTopics
+export default ListTopics;
