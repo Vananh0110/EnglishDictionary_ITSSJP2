a04d3f1bec88024151098d6539d48536a98cfc85
Phạm Vân Anh <85979283+Vananh0110@users.noreply.github.com>
2024-06-15 01:31:08 +0700

    Done: Detail words

--- frontend/src/pages/topic/pages/ListTopics.js
+++ frontend/src/pages/topic/pages/ListTopics.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import Layout from "../../../components/Layout";
-import { Col, Pagination, Row } from "antd";
-import axios from "../../../api/axios";
-import Topic from "../components/Topic";
+import Layout from '../../../components/Layout';
+import { Col, Pagination, Row } from 'antd';
+import axios from '../../../api/axios';
+import Topic from '../components/Topic';
 const ListTopics = () => {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -14,27 +14,29 @@ const ListTopics = () => {
 
   useEffect(() => {
     axios
-      .get("/topic")
+      .get('/topic')
       .then((response) => {
         setData(response.data);
       })
       .catch((error) => {
-        console.error("Error fetching data: ", error);
+        console.error('Error fetching data: ', error);
       });
   }, []);
 
   return (
-    <Layout page={"2"}>
-      <div className='container mx-auto h-full flex flex-col'>
-        <div className='w-full text-center text-5xl font-bold'>List Topics</div>
-        <Row gutter={[16, 16]} className='flex-1 mt-16'>
+    <Layout page={'2'}>
+      <div className="container mx-auto h-full flex flex-col">
+        <div className="w-full text-center text-5xl font-semibold">
+          List Topics
+        </div>
+        <Row gutter={[16, 16]} className="flex-1 mt-16">
           {data &&
             data?.map(
               (topic, index) =>
                 index >= 9 * currentPage - 9 &&
                 index < 9 * currentPage && (
                   <Col span={8} key={topic.id}>
-                    <div className='flex items-center justify-center p-2'>
+                    <div className="flex items-center justify-center p-2">
                       <Topic topic={topic} />
                     </div>
                   </Col>
@@ -42,7 +44,7 @@ const ListTopics = () => {
             )}
         </Row>
         <Pagination
-          className='w-full text-end mt-4 justify-self-end'
+          className="w-full text-end mt-4 justify-self-end"
           defaultPageSize={9}
           total={data?.length || 1}
           current={currentPage}
