3075a560c3f21716a47502ec33b2a87ee9209594
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2024-06-18 16:54:13 +0700

    fix detail page

--- frontend/src/pages/word/detail.js
+++ frontend/src/pages/word/detail.js
@@ -2,23 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
 import { useParams, Link } from 'react-router-dom';
 import axios from '../../api/axios';
-import { Input, Carousel, List } from 'antd';
+import { Carousel, List } from 'antd';
 import SearchComponent from '../../components/SearchComponent';
 import SearchImg from '../../assets/search.png';
-const { Search } = Input;
 const { Meta } = List.Item;
 
 const Detail = () => {
   const { id } = useParams();
   const [wordDetail, setWordDetail] = useState(null);
-  const [words, setWords] = useState([]);
-  const [searchResults, setSearchResults] = useState([]);
-  const [synonymLinks, setSynonymLinks] = useState([]);
-  const [antonymLinks, setAntonymLinks] = useState([]);
-
-  const handleSearch = (searchTerm) => {
-    console.log('Tìm kiếm cho:', searchTerm);
-  };
 
   useEffect(() => {
     const fetchWordDetail = async () => {
@@ -33,45 +24,7 @@ const Detail = () => {
     fetchWordDetail();
   }, [id]);
 
-  useEffect(() => {
-    const fetchWords = async () => {
-      try {
-        const response = await axios.get('/words');
-        setWords(response.data);
-      } catch (error) {
-        console.error('Failed to fetch words:', error);
-      }
-    };
-    fetchWords();
-  }, []);
-
-  useEffect(() => {
-    const findWordId = async (word) => {
-      try {
-        const response = await axios.get('/words');
-        const wordData = response.data.find((w) => w.word === word);
-        return wordData ? wordData.id : null;
-      } catch (error) {
-        console.error('Failed to fetch words:', error);
-        return null;
-      }
-    };
-
-    const getLinks = async (wordList) => {
-      const links = await Promise.all(
-        wordList.map(async (item) => {
-          const wordId = await findWordId(item.word);
-          return { ...item, wordId };
-        })
-      );
-      return links;
-    };
-
-    if (wordDetail) {
-      getLinks(wordDetail.tdn).then((links) => setSynonymLinks(links));
-      getLinks(wordDetail.ttn).then((links) => setAntonymLinks(links));
-    }
-  }, [wordDetail]);
+  console.log(wordDetail);
 
   if (!wordDetail) {
     return (
@@ -87,23 +40,18 @@ const Detail = () => {
     <Layout>
       <div className="pb-12">
         <div className="mb-10">
-          <SearchComponent onSearch={handleSearch} className="relative" />
+          <SearchComponent className="relative" />
           <img
             src={SearchImg}
             alt="icon"
             className="absolute  w-72 h-auto right-8 top-3"
           />
-          <div className="search-results">
-            {searchResults.map((result, index) => (
-              <div key={index}>{result}</div>
-            ))}
-          </div>
         </div>
         <div className="min-h-full flex">
           <div className="border-[#CED9E3] border-[3px] rounded-3xl hover:shadow-md bg-white p-4 me-10 w-1/4">
             <h3 className="text-xl font-semibold mb-2 mt-3">Synonyms</h3>
             <List
-              dataSource={synonymLinks}
+              dataSource={wordDetail?.tdn}
               renderItem={(item) => (
                 <List.Item className="hover:bg-slate-50">
                   <Meta
@@ -123,7 +71,7 @@ const Detail = () => {
             />
             <h3 className="text-xl font-semibold mb-2 mt-9">Antonyms</h3>
             <List
-              dataSource={antonymLinks}
+              dataSource={wordDetail?.ttn}
               renderItem={(item) => (
                 <List.Item className="hover:bg-slate-50">
                   <Meta
@@ -165,11 +113,14 @@ const Detail = () => {
               <div className="w-1/3 mt-10">
                 <Carousel autoplay arrows>
                   {wordDetail.image_link.map((link, index) => (
-                    <div key={index} className="relative h-80 flex items-center justify-center">
+                    <div
+                      key={index}
+                      className="relative h-80 flex items-center justify-center"
+                    >
                       <img
                         src={link}
                         alt={wordDetail.word}
-                        className="w-full h-full object-cover"
+                        className="w-full h-full object-contain"
                       />
                     </div>
                   ))}
